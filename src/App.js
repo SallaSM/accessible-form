4a7ed98a6fe023489dfebd8bfbfbabc018b1f60a
Salla <salla_mikkonen@hotmail.com>
2023-07-04 10:23:10 +0300

    Test 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test test test
+          Test test test test test 1
         </p>
         <a
           className="App-link"
